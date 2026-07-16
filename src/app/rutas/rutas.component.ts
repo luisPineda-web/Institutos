3192207d5e6bacf92e717994a4b99920b93e502c
Luis Pineda <luishernan22app@gmail.com>
2026-07-16 17:06:41 -0500

    Se está realizando un Slider

--- src/app/rutas/rutas.component.ts
+++ src/app/rutas/rutas.component.ts
@@ -10,4 +10,11 @@ import { RouterLinkActive } from '@angular/router';
 })
 export class RutasComponent {
 
+  slideIndex: number = 0;
+
+  cambiarSlide(direccion: number): void {
+    const total = 2; // porque solo tenemos dos imágenes
+    this.slideIndex = (this.slideIndex + direccion + total) % total;
+  }
+
 }
